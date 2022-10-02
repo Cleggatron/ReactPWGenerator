29ebfcf12612b2dfb390b12f8530b359d6efc219
Cleggatron <david.robert.clegg@gmail.com>
2022-10-02 18:49:29 +0100

    added task spec

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 
+/*
+Given any L, N, X, C return an L long string containing N numeric characters, X non alphabetic chars and C capital letters
+*/
+
 function App() {
   return (
     <div className="App">
